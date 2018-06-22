8819c13d7fba091698995bf6d86b91e959728e8d
Kshitij Gupta <kshitijgm@gmail.com>
2018-06-22 19:28:00 +0000

    vendor: Split SystemUIDark into Dark and Black
    - Now that we'll be respecting frameworks values as well, it's a good
      idea to give the user Dark/Black on QS as well.
    
    Change-Id: I63a43b46dc51e95594f6711eec38eb1b5a496ce3

--- config/packages.mk
+++ config/packages.mk
@@ -22,7 +22,8 @@ PRODUCT_PACKAGES += \
 # Themes Black
 PRODUCT_PACKAGES += \
     SettingsBlackTheme \
-    SystemBlackTheme
+    SystemBlackTheme \
+    SystemUIBlackTheme
 
 # Overlays
 PRODUCT_PACKAGES += \
