dac06af9d45a4faae9a3c2880411ba8d97340029
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-08-12 22:30:35 -0400

    vendor: Allow use of AOSP wlan hal on caf devices
    Change-Id: Icee822db334ee49bb2a17bff5e4a276437308138

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -90,7 +90,12 @@ $(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource
 $(call set-device-specific-path,POWER,power,hardware/qcom/power)
 
 $(call ril-set-path-variant,ril)
+ifeq ($(BOARD_USES_AOSP_WLAN_HAL),true)
+$(call wlan-set-path-variant,wlan)
+else
 $(call wlan-set-path-variant,wlan-caf)
+endif
+
 $(call bt-vendor-set-path-variant,bt-caf)
 
 else
