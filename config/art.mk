457cc638c9a7ea2c12482aa468c67d3446f2f573
mydongistiny <jaysonedson@gmail.com>
2018-08-14 02:08:22 -0700

    common: Include makefile with art definitions
    Change-Id: Ied15be62e315db3061dfb216ea6c7f0fdb43aa22
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: blinoff82 <blinov.in@gmail.com>

---
+++ config/art.mk
@@ -0,0 +1,25 @@
+# Don't build debug for host or device
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+ART_BUILD_TARGET_NDEBUG := true
+ART_BUILD_TARGET_DEBUG := false
+ART_BUILD_HOST_NDEBUG := true
+ART_BUILD_HOST_DEBUG := false
+
+# Dex pre-opt
+WITH_DEXPREOPT := true
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
+
+# Preopt apks like GMS chimara modules
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
+# Create profile for boot image
+# Outputs to /system/etc/boot-image.prof
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+
+# Speed apps
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+  SystemUI \
+  Settings
+
+# Dex pre-opt profiles
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed-profile
