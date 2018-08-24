shakhawat3742/vendor_aosp

[2/2] vendor: add OTA script do delete package cache
after flashing new image delete contents of /data/system/package_cache/
to make sure they are recreated. Outdated cache files are the
reason for the famous 'resource derps'

Change-Id: Iaf1f113a64242254b8fb33bc5b9fdef0c9fdb120

config/common.mk
@@ -23,7 +23,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/aosp/prebuilt/common/bin/sysinit:system/bin/sysinit
+    vendor/aosp/prebuilt/common/bin/sysinit:system/bin/sysinit \
+    vendor/aosp/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
 
 # Init file
 PRODUCT_COPY_FILES += \

syberia-project/platform_vendor_syberia

config/common.mk
@@ -34,21 +34,22 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_COPY_FILES += \
     vendor/syberia/prebuilt/common/bin/sysinit:system/bin/sysinit
-    
+
 # Init files
 
 PRODUCT_COPY_FILES += \
     vendor/syberia/prebuilt/common/etc/init.local.rc:system/etc/init/syberia.rc
-    
+
 # Don't export PS1 in /system/etc/mkshrc.
 PRODUCT_COPY_FILES += \
     vendor/syberia/prebuilt/common/etc/mkshrc:system/etc/mkshrc
-    
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/syberia/build/tools/50-syberia.sh:system/addon.d/50-syberia.sh \
     vendor/syberia/build/tools/backuptool.sh:install/bin/backuptool.sh \
     vendor/syberia/build/tools/backuptool.functions:install/bin/backuptool.functions \
+    vendor/syberia/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
 
 # Backup Services whitelist
 PRODUCT_COPY_FILES += \