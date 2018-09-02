a01c666340019ce2dc6be4ff662993a7d1bee8d5
Ivan <blinov.in@gmail.com>
2018-09-02 11:35:07 +0300

    Remove duplicate packages
    We have them in vendor/*/config/telephony.mk

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,11 +1,6 @@
 # Inherit common stuff
 $(call inherit-product, vendor/syberia/config/common.mk)
 
-# Telephony packages
-PRODUCT_PACKAGES += \
-    CellBroadcastReceiver \
-    Stk
-
 # Enable tethering
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.dun.override=0
