19813b4d88acf5aa6a72c6ef60acb005ec3406e2
DennySPB <dennyspb@gmail.com>
2018-09-21 10:50:07 +0300

    Simplify CAF targets
    We dropped AOSP RIL, BT, WLAN since Pie CAF HALs were realesed

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -37,16 +37,9 @@ $(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource
 $(call set-device-specific-path,POWER,power,hardware/qcom/power)
 
 $(call ril-set-path-variant,ril)
-ifeq ($(BOARD_USES_AOSP_WLAN_HAL),true)
 $(call wlan-set-path-variant,wlan)
-else
-$(call wlan-set-path-variant,wlan-caf)
-endif
+$(call bt-vendor-set-path-variant,bt)
 
-$(call bt-vendor-set-path-variant,bt-caf)
-
-PRODUCT_CFI_INCLUDE_PATHS += \
-    hardware/qcom/wlan-caf/qcwcn/wpa_supplicant_8_lib
 else
 
 $(call project-set-path,qcom-audio,hardware/qcom/audio/default)
