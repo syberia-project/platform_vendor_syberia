b1da7fc8712d3020978e9af8bbc902696e06a393
blinoff82 <blinov.in@gmail.com>
2018-09-29 14:15:00 +0300

    Add ASCII art to compilation completion

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -15,6 +15,27 @@
 # -----------------------------------------------------------------
 # Bacon update package
 
+# Localized color definitions
+# tput color defs:
+CLR_RED=$(shell tput setaf 1)
+CLR_GRN=$(shell tput setaf 2)
+CLR_YLW=$(shell tput setaf 3)
+CLR_BLU=$(shell tput setaf 4)
+CLR_MAG=$(shell tput setaf 5)
+CLR_CYN=$(shell tput setaf 6)
+CLR_WHT=$(shell tput setaf 7)
+CLR_BOLD=$(shell tput bold)
+CLR_RST=$(shell tput sgr0)
+
+# Shell color defs:
+CL_RED="\033[31m"
+CL_GRN="\033[32m"
+CL_YLW="\033[33m"
+CL_BLU="\033[34m"
+CL_MAG="\033[35m"
+CL_CYN="\033[36m"
+CL_RST="\033[0m"
+
 ifeq ($(TARGET_BACON_NAME),)
     INTERNAL_BACON_NAME := $(TARGET_PRODUCT)-$(PLATFORM_VERSION)-$(shell date -u +%Y%m%d)
 else
@@ -29,3 +50,21 @@ syberia: $(INTERNAL_OTA_PACKAGE_TARGET)
 	 $(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(INTERNAL_BACON_TARGET)
 	 $(hide) $(MD5SUM) $(INTERNAL_BACON_TARGET) | sed "s|$(PRODUCT_OUT)/||" > $(INTERNAL_BACON_TARGET).md5sum
 	 @echo "Package Complete: $(INTERNAL_BACON_TARGET)" >&2
+	 
+	 @echo -e ${CL_RST}"                                                                    "${CL_RST}
+	 @echo -e ${CL_RED}"                                                                    "${CL_RED}
+	 @echo -e ${CL_RED}"          ██████▓██   ██▓ ▄▄▄▄   ▓█████  ██▀███   ██▓ ▄▄▄           "${CL_RED}
+	 @echo -e ${CL_RED}"        ▒██    ▒ ▒██  ██▒▓█████▄ ▓█   ▀ ▓██ ▒ ██▒▓██▒▒████▄         "${CL_RED}
+	 @echo -e ${CL_RED}"        ░ ▓██▄    ▒██ ██░▒██▒ ▄██▒███   ▓██ ░▄█ ▒▒██▒▒██  ▀█▄       "${CL_RED}
+	 @echo -e ${CL_RED}"          ▒   ██▒ ░ ▐██▓░▒██░█▀  ▒▓█  ▄ ▒██▀▀█▄  ░██░░██▄▄▄▄██      "${CL_RED}
+	 @echo -e ${CL_RED}"       ▒██████▒▒ ░ ██▒▓░░▓█  ▀█▓░▒████▒░██▓ ▒██▒░██░ ▓█   ▓██▒      "${CL_RED}
+	 @echo -e ${CL_RED}"      ▒ ▒▓▒ ▒ ░  ██▒▒▒ ░▒▓███▀▒░░ ▒░ ░░ ▒▓ ░▒▓░░▓   ▒▒   ▓▒█░       "${CL_RED}
+	 @echo -e ${CL_RED}"      ░ ░▒  ░ ░▓██ ░▒░ ▒░▒   ░  ░ ░  ░  ░▒ ░ ▒░ ▒ ░  ▒   ▒▒ ░       "${CL_RED}
+	 @echo -e ${CL_RED}"	  ░  ░  ░  ▒ ▒ ░░   ░    ░    ░     ░░   ░  ▒ ░  ░   ▒          "${CL_RED}
+	 @echo -e ${CL_RST}"                                                                    "${CL_RST}
+	 @echo -e ${CL_RST}"         Build completed! Now flash that shit and enjoy!!           "${CL_RST}
+	 @echo -e ${CL_RST}"                                                                    "${CL_RST}
+	 @echo -e ${CL_RED}"===================================================================="${CL_RED}
+	 @echo -e ${CL_RST}"$(INTERNAL_BACON_TARGET)                                            "${CL_RST}
+	 @echo -e ${CL_RED}"===================================================================="${CL_RED}
+	 @echo -e ${CL_RST}"                                                                    "${CL_RST}
