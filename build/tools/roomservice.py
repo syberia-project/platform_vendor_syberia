0c36e9b39f620fbbda643b0bc213c02dedb8aa12
coderzstas <coderz35@gmail.com>
2019-03-14 16:29:04 +0300

    Roomservice: trim unnecessary device repo string

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -328,7 +328,7 @@ def main():
         print("Found repository: %s" % repository['name'])
 
         fallback_branch = detect_revision(repository)
-        manufacturer = repo_name[7:-(len(device)+1)]
+        manufacturer = repo_name.replace("platform_device_", "").replace("_" + device, "")
         repo_path = "device/%s/%s" % (manufacturer, device)
         adding = [{'repository': repo_name, 'target_path': repo_path}]
 
