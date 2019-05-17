00ce342f852025fdfbfb676b720b72465f5a3300
Rashed Abdel-Tawab <rashed@linux.com>
2019-05-17 12:03:57 -0700

    qcom: Add UM 4.14 platforms
    Change-Id: If246c0ec06de22097d369c17ce48aa1270bab19c

--- build/core/BoardConfigQcom.mk
+++ build/core/BoardConfigQcom.mk
@@ -6,6 +6,7 @@ B64_FAMILY := msm8992 msm8994
 BR_FAMILY := msm8909 msm8916
 UM_3_18_FAMILY := msm8937 msm8953 msm8996
 UM_4_4_FAMILY := msm8998 sdm660
+UM_4_14_FAMILY := sm8150 sm6150
 
 BOARD_USES_ADRENO := true
 
@@ -25,12 +26,12 @@ endif
 TARGET_USES_QCOM_MM_AUDIO := true
 
 # Enable color metadata for UM platforms
-ifeq ($(call is-board-platform-in-list, $(UM_3_18_FAMILY) $(UM_4_4_FAMILY)),true)
+ifeq ($(call is-board-platform-in-list, $(UM_3_18_FAMILY) $(UM_4_4_FAMILY)$(UM_4_14_FAMILY)),true)
     TARGET_USES_COLOR_METADATA := true
 endif
 
 # List of targets that use master side content protection
-MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660
+MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sm6150 sm8150
 ifeq ($(call is-board-platform-in-list, $(B_FAMILY)),true)
     MSM_VIDC_TARGET_LIST := $(B_FAMILY)
     QCOM_HARDWARE_VARIANT := msm8974
@@ -51,8 +52,13 @@ ifeq ($(call is-board-platform-in-list, $(UM_4_4_FAMILY)),true)
     MSM_VIDC_TARGET_LIST := $(UM_4_4_FAMILY)
     QCOM_HARDWARE_VARIANT := msm8998
 else
+ifeq ($(call is-board-platform-in-list, $(UM_4_14_FAMILY)),true)
+    MSM_VIDC_TARGET_LIST := $(UM_4_14_FAMILY)
+    QCOM_HARDWARE_VARIANT := sm8150
+else
     MSM_VIDC_TARGET_LIST := $(TARGET_BOARD_PLATFORM)
     QCOM_HARDWARE_VARIANT := $(TARGET_BOARD_PLATFORM)
+endif
 endif
 endif
 endif
