5c8909f311fca690c57e82bed8d6eeb9942b878e
DennySPB <dennyspb@gmail.com>
2019-05-20 13:12:19 +0300

    sdclang: switch to 8.0.6
    Change-Id: I38dac0a219e7e4e11de14a55be48277e3dfb693e
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- sdclang/vendorsetup.sh
+++ sdclang/vendorsetup.sh
@@ -1,6 +1,6 @@
 # Set SDClang defaults
 export SDCLANG=false
-export SDCLANG_PATH=vendor/qcom/sdclang-6.0/linux-x86/bin
+export SDCLANG_PATH=vendor/qcom/sdclang-8.0/bin
 export SDCLANG_LTO_DEFS=vendor/syberia/sdclang/sdllvm-lto-defs.mk
 export SDCLANG_COMMON_FLAGS="-O3 -fvectorize -Wno-user-defined-warnings -Wno-vectorizer-no-neon -Wno-unknown-warning-option \
 -Wno-deprecated-register -Wno-tautological-type-limit-compare -Wno-sign-compare -Wno-gnu-folding-constant \
