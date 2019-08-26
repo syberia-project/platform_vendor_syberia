d57c91aef7e5a3bb55029c9b3e323bbf29b47998
dennyspb <dennyspb@gmail.com>
2019-08-26 15:07:02 +0200

    Revert "Remove Google Markup prebuilt"
    This reverts commit f97112d633afbcb008bac4aabedfafc2249eea4a.
    
    Reason for revert: Longshot works well with Markup after granting storage permissions
    
    Change-Id: I8f4cc246e81bb248983d805f4ad9f5b2c026c7e3

--- prebuilt/common/apk/Android.mk
+++ prebuilt/common/apk/Android.mk
@@ -14,6 +14,17 @@
 
 LOCAL_PATH:= $(call my-dir)
 
+#Markup
+include $(CLEAR_VARS)
+LOCAL_MODULE := MarkupGoogle
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)
+
 #WallpaperPickerGoogle
 include $(CLEAR_VARS)
 LOCAL_MODULE := WallpaperPickerGoogle
