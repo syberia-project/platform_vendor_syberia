6c44c0d92e9ed007206f82121397a96c50f57afe
Sam Mortimer <sam@mortimer.me.uk>
2019-09-04 23:03:18 -0700

    vendor: Depend on existing definition of empty
    Defined and marked readonly in build/make here:
    https://github.com/LineageOS/android_build/blob/7ddc50a066287b804a754951a597668d627478df/common/core.mk#L46
    
    Change-Id: Ic233f0d41d03b28beb8d96c68692271d959fa188
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- build/core/qcom_utils.mk
+++ build/core/qcom_utils.mk
@@ -48,8 +48,8 @@ QSD8K_BOARD_PLATFORMS := qsd8k
 
 
 # vars for use by utils
-empty :=
-space := $(empty) $(empty)
+# the following are already defined in build/make:
+# empty space comma newline pound backslash
 colon := $(empty):$(empty)
 underscore := $(empty)_$(empty)
 
