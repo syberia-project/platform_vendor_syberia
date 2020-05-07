fd45cb1a163cdab53b32e785a1252d654ecfb39a
DennySPb <dennyspb@gmail.com>
2020-05-07 18:11:25 +0300

    vendor: treblize makefiles
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I1b6e2e1cf1f2819f8d1fc809ab9a71b727dd44c5

--- config/telephony.mk
+++ config/telephony.mk
@@ -3,15 +3,15 @@ $(call inherit-product, vendor/syberia/config/common.mk)
 
 # Sensitive Phone Numbers list
 PRODUCT_COPY_FILES += \
-    vendor/syberia/prebuilt/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+    vendor/syberia/prebuilt/etc/selective-spn-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/selective-spn-conf.xml
 
 # World APN list
 PRODUCT_COPY_FILES += \
-    vendor/syberia/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/syberia/prebuilt/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
 
 # World SPN overrides list
 PRODUCT_COPY_FILES += \
-    vendor/syberia/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml
+    vendor/syberia/prebuilt/etc/spn-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/spn-conf.xml
 
 # Telephony packages
 PRODUCT_PACKAGES += \
