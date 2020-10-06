6244fe1852677702011347d743fd2e409c123a75
DennySPb <dennyspb@gmail.com>
2020-10-06 11:08:08 +0300

    vendor: Allow hide gesture navbar [3/3]
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I73816fd4af64324e1038f15e4181a24cd101ba44

--- config/packages.mk
+++ config/packages.mk
@@ -117,4 +117,5 @@ PRODUCT_PACKAGES += \
 # Navbar
 PRODUCT_PACKAGES += \
     GesturalNavigationOverlayLong \
-    GesturalNavigationOverlayMedium
+    GesturalNavigationOverlayMedium \
+    GesturalNavigationOverlayHidden
