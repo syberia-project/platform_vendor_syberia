935dd9c3885c57fada5675024e1b514d890f4486
Agent24015 <norseloon44@gmail.com>
2021-02-23 17:27:07 +0000

    vendor: Introduce Switch styles [3/3]
    Change-Id: I632c2cde5345cf34cacf35aea3012b28f5deb588
    Signed-off-by: blinoff82 <blinov.in@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -118,6 +118,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     StitchImage
 
+# Switch styles
+PRODUCT_PACKAGES += \
+    SwitchAOSP \
+    SwitchTelegram \
+    SwitchMD2 \
+    SwitchOOS
+
 # Navbar
 PRODUCT_PACKAGES += \
     GesturalNavigationOverlayLong \
