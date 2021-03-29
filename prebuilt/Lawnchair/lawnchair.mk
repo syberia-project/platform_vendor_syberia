eea617025546996fb4943ca4d4d44f723e710ad9
Kshitij Gupta <kshitijgm@gmail.com>
2021-03-29 23:30:46 +0530

    prebuilts: Lawnchair: Move sysconfig and perms to system_ext
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I838f5c7dd024041353b828dfe8e1e71e860debf9

--- prebuilt/Lawnchair/lawnchair.mk
+++ prebuilt/Lawnchair/lawnchair.mk
@@ -4,5 +4,5 @@ LAWN_ROOT := vendor/syberia/prebuilt/Lawnchair
 PRODUCT_PACKAGES += Lawnchair
 PRODUCT_PACKAGE_OVERLAYS += $(LAWN_ROOT)/overlay
 PRODUCT_COPY_FILES += \
-    $(LAWN_ROOT)/permissions/privapp-permissions-lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-lawnchair.xml \
-    $(LAWN_ROOT)/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
+    $(LAWN_ROOT)/permissions/privapp-permissions-lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lawnchair.xml \
+    $(LAWN_ROOT)/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
