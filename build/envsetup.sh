b44dcb98c891cf2442f8d290e0cda8388fa2f9d7
Pranav Vashi <neobuddy89@gmail.com>
2021-05-19 18:01:18 +0300

    build: Enable Global ThinLTO and ThinLTO cache
    Change-Id: I7a163b11a54b2dbacc96ec9afddb1e1fba07e1d2

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -129,3 +129,8 @@ function repopick() {
         mkdir -p ${common_out_dir}
     fi
 }
+
+# Enable ThinLTO Source wide.
+echo "Building with ThinLTO."
+export GLOBAL_THINLTO=true
+export USE_THINLTO_CACHE=true
