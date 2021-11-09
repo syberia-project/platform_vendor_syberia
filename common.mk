22c01ee44b8216eb710b93aac355104ea0a48d22
kondors1995 <normandija1945@gmail.com>
2021-11-09 22:10:48 +0000

    vendor: remove audio
    we moved to a12 audio sounds in fwb
    
    Change-Id: Id71d11129233b4c01afdc1ebf29be27936b73aa8

--- common.mk
+++ common.mk
@@ -4,5 +4,3 @@ $(call inherit-product, vendor/syberia/config/common.mk)
 $(call inherit-product, vendor/syberia/config/telephony.mk)
 
 $(call inherit-product, vendor/syberia/config/syberia_props.mk)
-
-$(call inherit-product, vendor/syberia/audio/audio.mk)
