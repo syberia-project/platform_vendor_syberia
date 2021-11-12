5b615f2dcfb4bd093ac0e2e4fb606b64a4ef74c4
Danny Lin <danny@kdrag0n.dev>
2021-11-12 11:23:32 +0000

    Disable ABI checks
    ABI compatibility checks fail for several reasons:
      - The update to Clang 12 causes some changes, but no breakage has been
        observed in practice.
      - Switching to zlib-ng changes some internal structs, but not the public
        API.
    
    We may fix these eventually by updating the ABI specifications, but it's
    likely not worth the effort for us because of how many repos are affected.
    We would need to fork a lot of extra repos (thus increasing maintenance
    overhead) just to update the ABI specs.
    
    For now, just skip the ABI checks to fix build errors.
    
    Change-Id: I73f1b8ab2f85983f9cd9a86018967920fb0cb2c8

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -134,3 +134,6 @@ function repopick() {
 #echo "Building with ThinLTO."
 #export GLOBAL_THINLTO=true
 #export USE_THINLTO_CACHE=true
+
+# For now, just skip the ABI checks to fix build errors.
+export SKIP_ABI_CHECKS=true
