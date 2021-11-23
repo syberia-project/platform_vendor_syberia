ad075d3fcd3c8ccde3289cf6dd0f563595e6e29a
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-11-23 22:48:17 +0530

    syberia: don't dexpreopt prebuilts
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I746041231f2c3db00243cb7859474eaab80c863b

--- config/common.mk
+++ config/common.mk
@@ -15,6 +15,8 @@ endif
 # Optimize everything for preopt
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 ifeq ($(TARGET_SUPPORTS_64_BIT_APPS), true)
+DONT_DEXPREOPT_PREBUILTS := true
+
 # Use 64-bit dex2oat for better dexopt time.
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat64.enabled=true
