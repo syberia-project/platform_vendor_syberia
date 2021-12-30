1e48238971672310c97e39ab19d9ae6bfada5de7
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-12-30 22:58:36 +0900

    lawnchair: Import Lawnicons
     * HEAD as https://github.com/LawnchairLauncher/lawnicons/commit/6d5a6da2bd6b7c4789113524ad7405a0ef4d7088
    
    Change-Id: I5e2d2ebb9b20fd3929b9b0817827a518093046f3
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- prebuilt/Lawnchair/Android.mk
+++ prebuilt/Lawnchair/Android.mk
@@ -15,3 +15,13 @@ my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
 LOCAL_PREBUILT_JNI_LIBS := $(my_src_arch)/libnrb.so
 LOCAL_DEX_PREOPT := false
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := Lawnicons
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_SYSTEM_EXT_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)
--- prebuilt/Lawnchair/lawnchair.mk
+++ prebuilt/Lawnchair/lawnchair.mk
@@ -1,7 +1,7 @@
 # Lawnchair
 LAWN_ROOT := vendor/syberia/prebuilt/Lawnchair
 
-PRODUCT_PACKAGES += Lawnchair
+PRODUCT_PACKAGES += Lawnchair Lawnicons
 PRODUCT_PACKAGE_OVERLAYS += $(LAWN_ROOT)/overlay
 PRODUCT_COPY_FILES += \
     $(LAWN_ROOT)/permissions/privapp-permissions-lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lawnchair.xml \
