f6e6fdcdf2e9b91e1482d1736ff2f5006f773783
DennySPb <dennyspb@gmail.com>
2022-02-08 10:17:42 +0300

    Include GMS and Pixel makefiles
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I5d6c15e0ccc09f7903a39ccf3366f17d6cbe7e30
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- common.mk
+++ common.mk
@@ -5,4 +5,6 @@ $(call inherit-product, vendor/syberia/config/telephony.mk)
 
 $(call inherit-product, vendor/syberia/config/syberia_props.mk)
 
-$(call inherit-product, vendor/syberia/audio/audio.mk)
+$(call inherit-product, vendor/google/pixel/config.mk)
+
+$(call inherit-product, vendor/google/gms/config.mk)
