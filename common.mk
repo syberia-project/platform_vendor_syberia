381f7d91aad202fb56b5063a1666768a4903423b
kondors1995 <normandija1945@gmail.com>
2022-02-09 08:02:46 +0000

    syberia: Guard GMS include
    Change-Id: I9a4d6bd595ee33405641876fef86b8a3509502bb

--- common.mk
+++ common.mk
@@ -7,4 +7,6 @@ $(call inherit-product, vendor/syberia/config/syberia_props.mk)
 
 $(call inherit-product, vendor/google/pixel/config.mk)
 
+ifeq ($(WITH_GMS),true)
 $(call inherit-product, vendor/google/gms/config.mk)
+endif
