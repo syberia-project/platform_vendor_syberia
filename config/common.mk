b649744751b575da5f2569c4bf89f2fc15df58ab
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-07-15 10:58:14 -0400

    target: start dex-opting prebuilts
    * now that newer devices have begun migrating to EROFS
      we have the space to optimise prebuilt packages as well
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: Ide170c0717cbfd010f04a09e38d77e85edebb436

--- config/common.mk
+++ config/common.mk
@@ -8,9 +8,6 @@ include vendor/syberia/config/art.mk
 #PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 
 ifeq ($(TARGET_SUPPORTS_64_BIT_APPS), true)
-# Don't preopt prebuilts
-DONT_DEXPREOPT_PREBUILTS := true
-
 # Use 64-bit dex2oat for better dexopt time.
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat64.enabled=true
