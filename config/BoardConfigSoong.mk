4bf32cb23d34ef5765179aeddf3120d489e60264
Bruno Martins <bgcngm@gmail.com>
2023-05-16 21:40:28 +0100

    Hookup GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE
    The gralloc struct has been conditionally adjusted not to
    break blobs that use the old struct.
    
    So far only GRALLOC_HANDLE_HAS_RESERVED_SIZE was hooked up
    for Google Pixels, but it's now time to hook up the remaining flag.
    
    Change-Id: I175bd328bd9733b9487e349f7a8486f15ef0a450

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -37,6 +37,7 @@ SOONG_CONFIG_aospGlobalVars += \
     uses_nothing_camera \
     ignores_ftp_pptp_conntrack_failure \
     needs_netd_direct_connect_rule \
+    gralloc_handle_has_custom_content_md_reserved_size \
     gralloc_handle_has_reserved_size \
     target_init_vendor_lib \
     target_ld_shim_libs \
@@ -65,6 +66,7 @@ SOONG_CONFIG_aospQcomVars += \
 endif
 
 # Soong bool variables
+SOONG_CONFIG_aospGlobalVars_gralloc_handle_has_custom_content_md_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE)
 SOONG_CONFIG_aospGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
 SOONG_CONFIG_aospGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_aospGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
@@ -83,7 +85,9 @@ SOONG_CONFIG_aospGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_aospGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 
 # Set default values
+TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE ?= false
+TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE ?= false
 BOOTLOADER_MESSAGE_OFFSET ?= 0
 TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY ?= libcamera_parameters
