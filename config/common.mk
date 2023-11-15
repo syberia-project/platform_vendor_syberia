d935a5baac9ad946509bfb664b5d7e7eeae5eac1
ponces <ponces26@gmail.com>
2023-11-15 16:02:38 +0000

    Switch to AOSPA's face unlock implementation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -159,12 +159,13 @@ PRODUCT_PACKAGES += \
     product_charger_res_images
 endif
 
-TARGET_FACE_UNLOCK_SUPPORTED ?= true
-ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
 PRODUCT_PACKAGES += \
-    FaceUnlockService
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+    FaceUnlock
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
