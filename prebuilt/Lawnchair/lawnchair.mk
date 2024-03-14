35a18f8bc2fb0b0c5e86b4559a36ac99be21538b
DennySPb <DennySPb@gmail.com>
2024-03-14 11:49:44 +0300

    General cleanup
    Change-Id: I46a6ea600a6304216419b5880c0f2cc3847aed54

--- prebuilt/Lawnchair/lawnchair.mk
+++
@@ -1,8 +0,0 @@
-# Lawnchair
-LAWN_ROOT := vendor/syberia/prebuilt/Lawnchair
-
-PRODUCT_PACKAGES += Lawnchair LawnchairOverlay
-PRODUCT_PACKAGE_OVERLAYS += $(LAWN_ROOT)/overlay
-PRODUCT_COPY_FILES += \
-    $(LAWN_ROOT)/permissions/privapp-permissions-lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lawnchair.xml \
-    $(LAWN_ROOT)/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
