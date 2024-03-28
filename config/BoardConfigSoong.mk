c4f82954e3ae917fa997dd6cae4a2302dde2b53c
Jake Weinstein <jake@aospa.co>
2024-03-28 10:29:51 +0000

    soong: Add camera package name
    Use TARGET_CAMERA_PACKAGE_NAME to specify
    the package name.
    
    Suggested-By: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I9768d0ac40517b6555fd0606b299760f5baed71b
    Signed-off-by: erwinabs <erwinabubakarsidik@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -48,7 +48,8 @@ SOONG_CONFIG_aospGlobalVars += \
     target_process_sdk_version_override \
     target_surfaceflinger_udfps_lib \
     uses_camera_parameter_lib \
-    needs_camera_boottime
+    needs_camera_boottime \
+    target_camera_package_name
 
 SOONG_CONFIG_NAMESPACES += aospNvidiaVars
 SOONG_CONFIG_aospNvidiaVars += \
@@ -97,6 +98,7 @@ SOONG_CONFIG_aospGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_aospGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
 SOONG_CONFIG_aospGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
 SOONG_CONFIG_aospGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_aospGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 
 # Set default values
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
