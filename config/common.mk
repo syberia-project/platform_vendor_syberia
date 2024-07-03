bb8783a5a81950be15b7be382075a3ae135f6c20
DennySPb <DennySPb@gmail.com>
2024-07-03 10:31:13 +0300

    Revert "vendor: Ignore neverallows on user builds to"
    This reverts commit 7ad9e728aeea776ec340ec47a354da16cc48a31e.

--- config/common.mk
+++ config/common.mk
@@ -15,7 +15,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     vendor/syberia/overlay/common
 
 # We modify several neverallows, so let the build proceed
-ifneq ($(TARGET_BUILD_VARIANT),eng)
+ifneq ($(TARGET_BUILD_VARIANT),user)
 SELINUX_IGNORE_NEVERALLOWS := true
 endif
 
